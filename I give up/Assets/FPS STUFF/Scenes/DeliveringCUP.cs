4028e494e9eafec708c10a91dfd183e9e335c573
Zhi563 <33348173+Zhi563@users.noreply.github.com>
2018-12-09 11:18:59 -0500

    The main file for everything
    Things

---
+++ I give up/Assets/FPS STUFF/Scenes/DeliveringCUP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeliveringCUP : MonoBehaviour {
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+
+	// Use this for initialization
+	void Start () {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        Debug.Log("asdf");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("mouse down");
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;
+    }
+    void OnMouseUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position = guide.transform.position;
+    }
+}
